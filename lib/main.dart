30772f9746420331684b27abedb488a32deb64e1
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-04-17 07:44:46 -0300

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tasklist/views/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const TaskListApp());
 }

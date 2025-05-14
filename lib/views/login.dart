94d7e2664e6c66abeba0d62e25c338617d1399d3
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-05-14 07:45:58 -0300

    Add Firebase Auth

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,10 +1,27 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, must_be_immutable
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   var txtEmail = TextEditingController();
   var txtSenha = TextEditingController();
+
+  var firebaseAuth = FirebaseAuth.instance;
+
+  void login(BuildContext context) async {
+    try {
+      await firebaseAuth.signInWithEmailAndPassword(
+        email: txtEmail.text,
+        password: txtSenha.text);
+
+      Navigator.pushNamed(context, "/list");
+    }
+    on FirebaseAuthException catch(ex) {
+      SnackBar snackBar = SnackBar(content: Text(ex.message!));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +41,7 @@ class LoginPage extends StatelessWidget {
             SizedBox(height: 10,),
             TextField(
               controller: txtSenha,
+              obscureText: true,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: "Senha",
@@ -31,7 +49,7 @@ class LoginPage extends StatelessWidget {
             ),
             SizedBox(height: 10,),
             ElevatedButton(
-              onPressed: (){},
+              onPressed: () => login(context),
               child: Text("Login"),
             ),
             SizedBox(height: 10,),
--- lib/views/registro.dart
+++ lib/views/registro.dart
@@ -1,10 +1,27 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, must_be_immutable
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegistroPage extends StatelessWidget {
   var txtEmail = TextEditingController();
   var txtSenha = TextEditingController();
+
+  var firebaseAuth = FirebaseAuth.instance;
+
+  void registro(BuildContext context) async {
+    try {
+      await firebaseAuth.createUserWithEmailAndPassword(
+        email: txtEmail.text,
+        password: txtSenha.text);
+
+      Navigator.pushNamed(context, "/list");
+    }
+    on FirebaseAuthException catch(ex) {
+      SnackBar snackBar = SnackBar(content: Text(ex.message!));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +48,7 @@ class RegistroPage extends StatelessWidget {
             ),
             SizedBox(height: 10,),
             ElevatedButton(
-              onPressed: (){},
+              onPressed: () => registro(context),
               child: Text("Registar"),
             ),
             SizedBox(height: 10,),
